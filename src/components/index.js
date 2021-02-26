d804cf7a4070431c24728b778821197d41f7c560
cantwell <rjazantsev@gmail.com>
2021-02-26 09:42:11 +0300

    export Favourite

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
-export { default as Header } from './Header.jsx';
-export { default as Search } from './Search.jsx';
+export { default as Header } from './Header';
+export { default as Search } from './Search.old';
+export { default as Favourite } from './Favorite';
