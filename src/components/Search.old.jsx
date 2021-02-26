017255adaf68f936a964c433e8c153547961d469
cantwell <rjazantsev@gmail.com>
2021-02-26 09:38:04 +0300

    rename file

---
+++ src/components/Search.old.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Search = () => {
+    return (        
+        <main className="content">
+            <h1>Поиск видео</h1>
+            <form className="form">
+                <div className="united-search">
+                    <input className="search" type="text" placeholder="Что хотите посмотреть?" />
+                    <button className="button button--fill">Найти</button>
+                </div>
+            </form>
+        </main>
+    );
+}
+
+export default Search;
