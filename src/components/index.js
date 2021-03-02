d0cec421815dfb3d727ba5d4e5bda1367943195c
cantwell <rjazantsev@gmail.com>
2021-03-02 11:30:07 +0300

    export components Button DisplayGrid & DisplayList

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Header } from './Header';
 export { default as Search } from './Search';
 export { default as Favourite } from './Favorite';
+export { default as Button } from './Button';
+export { default as DisplayGrid } from './DisplayGrid';
+export { default as DisplayList } from './DisplayList';
