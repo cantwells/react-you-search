0ee214423bd97221281eb3469fd0ec6148e1a206
cantwell <rjazantsev@gmail.com>
2021-03-04 16:11:51 +0300

    add helper func multyColorRange

--- src/helper.js
+++ src/helper.js
@@ -12,6 +12,10 @@ const helper = {
         const re = /&#?\w{2,4};/gi;
     
         return str.replace(re, (match) => map[match])
+    },
+    multyColorRange: (event) => {
+        const target = event.target;
+        target.style.background = 'linear-gradient(to right, #1390E5 0%, #1390E5 ' + ( target.value * 2) + '%, #EAEAEA ' +  ( target.value * 2) + '%, #EAEAEA 100%)'
     }
 }
 
