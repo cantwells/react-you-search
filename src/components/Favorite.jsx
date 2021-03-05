b6d4a216bbac3e0696f81ed0dfa97575afffc82b
cantwell <rjazantsev@gmail.com>
2021-03-05 15:29:03 +0300

    output favourite items from state

--- src/components/Favorite.jsx
+++ src/components/Favorite.jsx
@@ -1,29 +1,22 @@
+import { useSelector } from "react-redux";
+
 const Favourite = () => {
+    const items = useSelector(({favourites}) => favourites.items)
     return (
         <main className="main main-favourits">
             <h2>Избранное</h2>
             <div className="favourits">
-                <div className="item">
-                    <div className="item__title">чем кормить кота</div>
-                    <div className="item__edit">
-                        <span>Изменить</span>
-                        <span className="item__edit-del">Удалить</span>
-                    </div>
-                </div>
-                <div className="item">
-                    <div className="item__title">видео</div>
-                    <div className="item__edit">
-                        <span>Изменить</span>
-                        <span className="item__edit-del">Удалить</span>
-                    </div>
-                </div>
-                <div className="item">
-                    <div className="item__title">bad bunny</div>
-                    <div className="item__edit">
-                        <span>Изменить</span>
-                        <span className="item__edit-del">Удалить</span>
-                    </div>
-                </div>
+                {
+                    items.map( item => (
+                        <div className="item">
+                            <div className="item__title">{item.request}</div>
+                            <div className="item__edit">
+                                <span>Изменить</span>
+                                <span className="item__edit-del">Удалить</span>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </main>
     )
