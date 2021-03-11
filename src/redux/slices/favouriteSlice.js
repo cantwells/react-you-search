8910358aa58f4f18e563e92f50a934fad10135eb
cantwell <rjazantsev@gmail.com>
2021-03-11 09:16:41 +0300

    add action delFavourite

--- src/redux/slices/favouriteSlice.js
+++ src/redux/slices/favouriteSlice.js
@@ -8,9 +8,13 @@ const favouriteSlice = createSlice({
     reducers: {
         addFavourite( state, action ){
             state.items.push(action.payload);
+        },
+        delFavourite( state, action ){
+            const index = state.items.findIndex( item => item.id === action.payload.id );
+            state.items.splice( index, 1 );
         }
     }
 })
 
-export const { addFavourite } = favouriteSlice.actions;
+export const { addFavourite, delFavourite } = favouriteSlice.actions;
 export default favouriteSlice.reducer;
