bd59d198401ebe6a2778c538188d635027887a7b
cantwell <rjazantsev@gmail.com>
2021-03-17 10:47:57 +0300

    refactoring routing in index js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux'
 import './scss/styles.scss';
 import store from './redux/store';
+import { Login } from './components';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={App} />
+        </Switch>
       </Router>
     </Provider>
   </React.StrictMode>,
