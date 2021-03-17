461d6269d8c6d3510afeb9636a16ef5dd47394f1
cantwell <rjazantsev@gmail.com>
2021-03-17 10:39:37 +0300

    refactoring routing in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Favourite, Header, Search } from './components';
+import { Favourite, Header, Nomatch, Search } from './components';
 
 function App() {
   return (
     <div className="wrapper">
       <Header/>
-      <Route exact path="/" component={Search}/>
-      <Route exact path="/favourite" component={Favourite}/>
+      <Switch>
+        <Route exact path="/" component={Search}/>
+        <Route path="/favourite" component={Favourite}/>
+        <Route path="*" component={Nomatch}/>
+      </Switch>
     </div>
   );
 }
