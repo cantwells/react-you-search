0f70b4d7409ddf3600e1674615838f8441065b90
cantwell <rjazantsev@gmail.com>
2021-03-22 14:59:35 +0300

    output modalFavourite

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from "react-router-dom";
-import { Button, DisplayGrid, DisplayList, EmptySearch, ModalAddFavourite } from '.';
+import { Button, DisplayGrid, DisplayList, EmptySearch, ModalAddFavourite, ModalFavourite } from '.';
 import cn from 'classnames';
 import { fetchVideosByQuery, setIsGrid } from '../redux/slices/searchSlice';
 import { addFavourite } from '../redux/slices/favouriteSlice';
@@ -9,18 +8,42 @@ import { addFavourite } from '../redux/slices/favouriteSlice';
 let query = '';
 
 const Search = React.memo(() => {
+    const dispatch = useDispatch();
+    //получить данные из стейта
+    const {videos, totalResult, isGrid} = useSelector(({search}) => search);
+    const favourites = useSelector(({favourites}) => favourites.items);
+
     //Получение данных из поисковой строки
     const [ value, setValue ] = React.useState("");
-    //отображать/скрывать форму
-    const [ modalShow, setModalShow ] = React.useState(false);
 
-    const dispatch = useDispatch();
-    const {videos, totalResult, isGrid} = useSelector(({search}) => search);
+    //отображать/скрывать модалку с формой
+    const [ modalShow, setModalShow ] = React.useState(false);
+    //отображать/скрывать модалку для перехода на избранное
+    const [ modalFavourite, setModalFavourite ] = React.useState(false);
+
+    //проверка вводимой строки на наличие в избранном
+    const isFavourite = () => {
+        return !!favourites.find( item => item.request === value );
+    }
 
     //Обработчик ввода данных в поисковой строке
     const handleGetValue = (event) => {
         setValue(event.target.value);
     }
+
+    //Обработчик для отображения модального окна с преходом в избранное
+    const handleModalFavourite = () => {
+        setModalFavourite(true);
+        setTimeout(() => {
+            setModalFavourite(false);
+        }, 3600)
+    }
+
+    const handleHideFavourite = () => {
+        console.log('stop');
+        // setModalFavourite(false);
+    };
+
     //Обработчик отправки формы в поисковой строке
     const handleSubmit = (event) => {
         if( value ) {
@@ -35,9 +58,10 @@ const Search = React.memo(() => {
     }
     //Показывать/скрывать модальное окно
     const handleModalShow = ( isShow ) => {
-        setModalShow(isShow);
+        isFavourite() ? handleModalFavourite() : setModalShow(isShow);
     }
 
+    //Добавление в избранное
     const handleAddFavourite = ( data ) => {
         dispatch(addFavourite(data));
     }
@@ -56,14 +80,13 @@ const Search = React.memo(() => {
                                             value={value}
                                             onChange={handleGetValue}
                                         />
-                                    <svg onClick={() => handleModalShow(true)} className="search__heart" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    {/* <svg onClick={() => handleModalShow(true)} className="search__heart" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> */}
+                                    <svg onClick={() => handleModalShow(true)} className={cn('search__heart', {'search__heart--fill': isFavourite()}) } viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M20.8401 4.60999C20.3294 4.099 19.7229 3.69364 19.0555 3.41708C18.388 3.14052 17.6726 2.99817 16.9501 2.99817C16.2276 2.99817 15.5122 3.14052 14.8448 3.41708C14.1773 3.69364 13.5709 4.099 13.0601 4.60999L12.0001 5.66999L10.9401 4.60999C9.90843 3.5783 8.50915 2.9987 7.05012 2.9987C5.59109 2.9987 4.19181 3.5783 3.16012 4.60999C2.12843 5.64169 1.54883 7.04096 1.54883 8.49999C1.54883 9.95903 2.12843 11.3583 3.16012 12.39L4.22012 13.45L12.0001 21.23L19.7801 13.45L20.8401 12.39C21.3511 11.8792 21.7565 11.2728 22.033 10.6053C22.3096 9.93789 22.4519 9.22248 22.4519 8.49999C22.4519 7.77751 22.3096 7.0621 22.033 6.39464C21.7565 5.72718 21.3511 5.12075 20.8401 4.60999V4.60999Z" stroke="#1390E5" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                                     </svg>
                                     <Button className="button--fill button--search">Найти</Button>
-                                    <div className="modal__favorite-saved">
-                                        <p>Поиск сохранён в разделе «Избранное»</p>
-                                        <Link to="/favourite">Перейти в избранное</Link>
-                                    </div>
+                                    {/* { modalFavourite && <ModalFavourite onHideFavourite={handleHideFavourite} /> } */}
+                                    {<ModalFavourite isShow={modalFavourite } onHideFavourite={handleHideFavourite} /> }
                                 </div>
                             </form>
                         </section>
@@ -99,11 +122,12 @@ const Search = React.memo(() => {
                     </main>
                 : <EmptySearch value={value} onSubmit={handleSubmit} onGetValue={handleGetValue} />
             }
-            <ModalAddFavourite isShow={modalShow} 
-                                onModalShow={handleModalShow} 
+            {
+                modalShow && <ModalAddFavourite  onModalShow={handleModalShow} 
                                 query={query}
                                 onAddFavourite={handleAddFavourite}
                             />
+            }
         </>
     )
 });
