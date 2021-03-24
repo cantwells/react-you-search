5045b1ea052ebc1f82df192f9e89a5d865bed63e
cantwell <rjazantsev@gmail.com>
2021-03-24 09:20:12 +0300

    wrapped ModalFavourite in React.memo

--- src/components/ModalFavourite.jsx
+++ src/components/ModalFavourite.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import cn from 'classnames'
 
-const ModalFavourite = ( {onHideFavourite, isShow} ) => {
+const ModalFavourite = React.memo(( {onHideFavourite, isShow} ) => {
     return (
         <div className={ cn('modal__favorite_saved', { 'modal__favorite_saved--show': isShow })}>
             <p>Поиск сохранён в разделе «Избранное»</p>
             <Link to="/favourite" onClick={onHideFavourite}>Перейти в избранное</Link>
         </div>
     )
-}
+});
 
 export default ModalFavourite;
