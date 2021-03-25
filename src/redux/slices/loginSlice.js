330e8ebe7d55bc38343cca946fd701092dc84ea6
cantwell <rjazantsev@gmail.com>
2021-03-25 12:20:48 +0300

    adding thunk fetchLogin and resetError actions

--- src/redux/slices/loginSlice.js
+++ src/redux/slices/loginSlice.js
@@ -1,19 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import helper from "../../helper";
+
+export const fetchLogin = createAsyncThunk(
+    'login/fetchLogin',
+    async ( credentials, thunkAPI ) => {
+        try{
+            console.log(thunkAPI.getState());
+            return await helper.logIn(credentials);
+        }catch(err){
+            throw err;
+        }
+    }
+)
 
 const loginSlice = createSlice({
     name: 'login',
     initialState: {
         isAuthorized: false,
+        error: "",
     },
     reducers: {
         setAuthorized( state, action ){
             state.isAuthorized = action.payload.isAuthorized;
         },
-        logOut(state, action){
+        logOut(state){
             state.isAuthorized = false;
+        },
+        resetError( state ){
+            state.error = ""
         }
     },
+    extraReducers: {
+        [fetchLogin.fulfilled]: ( state ) => {
+            state.isAuthorized = true;
+            state.error = "";
+        },
+        [fetchLogin.rejected]: ( state, action ) => {
+            state.error = action.error.message;
+        }
+    }
 })
 
-export const { setAuthorized, logOut } = loginSlice.actions;
+export const { setAuthorized, logOut, resetError } = loginSlice.actions;
 export default loginSlice.reducer;
