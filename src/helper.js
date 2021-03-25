db56c3ffae1c2f4974a302da049248343bc14fdd
cantwell <rjazantsev@gmail.com>
2021-03-25 09:26:50 +0300

    adding help func logIn

--- src/helper.js
+++ src/helper.js
@@ -16,6 +16,17 @@ const helper = {
     multyColorRange: (event) => {
         const target = event.target;
         target.style.background = 'linear-gradient(to right, #1390E5 0%, #1390E5 ' + ( target.value * 2) + '%, #EAEAEA ' +  ( target.value * 2) + '%, #EAEAEA 100%)'
+    },
+    logIn: ( credentials ) => {
+        const { username, password } = credentials;
+        return new Promise( ( resolve, reject ) => {
+            if( username === 'Admin' && password === '12345' ){
+                resolve(true);
+            }else{
+                reject("Не верный логин или пароль!");
+            }
+
+        })
     }
 }
 
