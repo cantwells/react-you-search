a8b06855cfb07e89901f13322a991620afcbe144
cantwell <rjazantsev@gmail.com>
2021-03-29 15:10:19 +0300

    add close button in MoadalFavourite

--- src/components/ModalFavourite.jsx
+++ src/components/ModalFavourite.jsx
@@ -5,8 +5,10 @@ import cn from 'classnames'
 const ModalFavourite = React.memo(( {onHideFavourite, isShow} ) => {
     return (
         <div className={ cn('modal__favorite_saved', { 'modal__favorite_saved--show': isShow })}>
+            <span role="button" tabIndex="0" aria-label="Закрыть" onClick={onHideFavourite}>X</span>
             <p>Поиск сохранён в разделе «Избранное»</p>
-            <Link to="/favourite" onClick={onHideFavourite}>Перейти в избранное</Link>
+            <Link to="/favourite">Перейти в избранное</Link>
+            {/* <p onClick={onHideFavourite}>Перейти в избранное</p> */}
         </div>
     )
 });
