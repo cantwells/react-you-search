1a59a1eb2f878f622561b6ee2654ce193e0f6f04
cantwell <rjazantsev@gmail.com>
2021-03-29 11:29:49 +0300

    serialization data and load to localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,22 @@ import { configureStore } from '@reduxjs/toolkit';
 import favourites from './slices/favouriteSlice';
 import search from './slices/searchSlice';
 import login from './slices/loginSlice';
+import browserStorage from '../browserStorage';
+import helper from '../helper';
 
-export default configureStore({
+const store = configureStore({
     reducer: {
         search,
         favourites,
         login
     }
 })
+
+store.subscribe( () => {
+    if( store.getState().login.isAuthorized ){
+        const serializedData = helper.utf8_to_b64(JSON.stringify(store.getState().login));
+        browserStorage.saveData(serializedData);
+    }
+} )
+
+export default store;
