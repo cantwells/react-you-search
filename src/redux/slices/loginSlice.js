b6e4ad2bc0b07df629fdceaf82244a727658c67e
cantwell <rjazantsev@gmail.com>
2021-03-29 11:30:56 +0300

    decode data from localStorage and add in slice

--- src/redux/slices/loginSlice.js
+++ src/redux/slices/loginSlice.js
@@ -1,9 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import browserStorage from "../../browserStorage";
 import helper from "../../helper";
 
+//thunk для проверки авторизационных данных
 export const fetchLogin = createAsyncThunk(
     'login/fetchLogin',
-    async ( credentials, thunkAPI ) => {
+    async ( credentials ) => {
         try{
             return await helper.logIn(credentials);
         }catch(err){
@@ -11,27 +13,36 @@ export const fetchLogin = createAsyncThunk(
         }
     }
 )
+//получаем данные из localStorage
+const data = browserStorage.getData('token');
+//Либо декодируем в объект либо присваиваем пустой объект, которые потом передаём в качестве initialState
+const persistedState = Object.keys(data).length ? JSON.parse(helper.b64_to_utf8(data)) : {};
 
 const loginSlice = createSlice({
     name: 'login',
-    initialState: {
-        isAuthorized: false,
-        error: "",
-    },
+    // initialState: {
+    //     isAuthorized: false,
+    //     user: {},
+    //     error: "",
+    // },
+    initialState: persistedState,
     reducers: {
         setAuthorized( state, action ){
             state.isAuthorized = action.payload.isAuthorized;
         },
         logOut(state){
             state.isAuthorized = false;
+            state.user = {}
         },
         resetError( state ){
             state.error = ""
         }
     },
     extraReducers: {
-        [fetchLogin.fulfilled]: ( state ) => {
+        [fetchLogin.fulfilled]: ( state, action ) => {
+            console.log(action);
             state.isAuthorized = true;
+            state.user = action.payload;
             state.error = "";
         },
         [fetchLogin.rejected]: ( state, action ) => {
