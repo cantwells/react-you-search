4bad2b6e591df5f9076cd0cc316ff41231dd67c7
cantwell <rjazantsev@gmail.com>
2021-04-02 15:40:50 +0300

    Deleting close element in InfoModa

--- src/components/InfoModal.jsx
+++ src/components/InfoModal.jsx
@@ -5,9 +5,8 @@ import cn from 'classnames'
 const InfoModal = React.memo(( {onHideModal, isShow} ) => {
     return (
         <div className={ cn('modal__info', { 'modal__info--show': isShow })}>
-            <span role="button" tabIndex="0" aria-label="Закрыть" onClick={onHideModal}>X</span>
             <p>Поиск сохранён в разделе «Избранное»</p>
-            <Link to="/favourite">Перейти в избранное</Link>
+            <Link onClick={onHideModal} to="/favourite">Перейти в избранное</Link>
         </div>
     )
 });
