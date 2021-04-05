ee9b85afcca5bb8406ca53be2cca706dd4cb4b3b
cantwell <rjazantsev@gmail.com>
2021-04-05 14:06:35 +0300

    Changing getting object instead primitive in fetchVideos

--- src/redux/dal/api.js
+++ src/redux/dal/api.js
@@ -3,8 +3,8 @@ const API_KEY = 'AIzaSyAQdRphaSUuFoKjC7O_UF4IK5d1pcEw5QU';
 const URI = 'https://www.googleapis.com/youtube/v3/';
 
 const API = {
-    fetchVideos: ( query, amount = 12, sort = 'relevance' ) => {
-        return axios.get(`${URI}search?part=snippet&q=${query}&type=video&maxResults=${amount}&order=${sort}&key=${API_KEY}`);
+    fetchVideos: ( {request, amount, sort } ) => {
+        return axios.get(`${URI}search?part=snippet&q=${request}&type=video&maxResults=${amount || 12}&order=${sort || 'relevance'}&key=${API_KEY}`);
     },
     getViewCount: ( videoId ) => {
         return axios.get(`${URI}videos?part=statistics&id=${videoId}&key=${API_KEY}`);
