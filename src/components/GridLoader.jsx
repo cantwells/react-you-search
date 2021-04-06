2263b0acce678b532b211a637b59943fd97e540b
cantwell <rjazantsev@gmail.com>
2021-04-06 11:55:20 +0300

    Adding loader for display Grid

---
+++ src/components/GridLoader.jsx
@@ -0,0 +1,18 @@
+import ContentLoader from "react-content-loader";
+
+const GridLoader = () => (
+    <ContentLoader 
+    speed={2}
+    width={245}
+    height={220}
+    // viewBox="0 0 245 220"
+    backgroundColor="#e6e6e6"
+    foregroundColor="#ecebeb"
+    >
+    <rect x="0" y="0" rx="0" ry="0" width="245" height="138" /> 
+    <rect x="0" y="147" rx="0" ry="0" width="245" height="32" /> 
+    <rect x="0" y="186" rx="0" ry="0" width="245" height="16" />
+    </ContentLoader>
+)
+
+export default GridLoader;
--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as Search } from './Search';
 export { default as Favourite } from './Favorite';
 export { default as Button } from './Button';
 export { default as DisplayGrid } from './DisplayGrid';
+export { default as GridLoader } from './GridLoader.jsx';
 export { default as DisplayList } from './DisplayList';
 export { default as ModalFavourite } from './ModalFavourite';
 export { default as EmptySearch } from './EmptySearch';
