9e28e2e96f0d2a8034252f154117d2b171a40564
cantwell <rjazantsev@gmail.com>
2021-05-19 15:27:25 +0300

    Refactoring adaptivity in movie-card & preloader

--- src/components/GridLoader.jsx
+++ src/components/GridLoader.jsx
@@ -2,15 +2,13 @@ import ContentLoader from "react-content-loader";
 
 const GridLoader = () => (
     <ContentLoader 
-    speed={2}
-    width={245}
-    height={220}
-    backgroundColor="#e6e6e6"
-    foregroundColor="#ecebeb"
-    >
-    <rect x="0" y="0" rx="0" ry="0" width="245" height="138" /> 
-    <rect x="0" y="147" rx="0" ry="0" width="245" height="32" /> 
-    <rect x="0" y="186" rx="0" ry="0" width="245" height="16" />
+        speed={2}
+        className="item"
+        backgroundColor="#e6e6e6"
+        foregroundColor="#ecebeb"
+        >
+        <rect x="0" y="0" rx="0" ry="0" width="100%" height="118" /> 
+        <rect x="0" y="123" rx="0" ry="0" width="100%" height="32" /> 
     </ContentLoader>
 )
 
